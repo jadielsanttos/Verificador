a1b33a4be3a7fc3be3f31ff8aceabecf789976b1
Jadiel Santos <82414367+jadielsanttos@users.noreply.github.com>
2021-10-19 08:57:23 -0300

    Add files via upload

---
+++ verificador de idades/script.js
@@ -0,0 +1,58 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('[ERRO] verifique os dados e tente novamente!')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <= 10) {
+                //criança
+                img.setAttribute('src', 'crianca-masc.png')
+                img.style.marginTop = '10px'
+            }else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'homem-jovem.png')
+                img.style.marginTop = '10px'
+            }else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homem-adulto.png')
+                img.style.marginTop = '10px'
+            }else {
+                //idoso
+                img.setAttribute('src', 'idoso-masc.png')
+                img.style.marginTop = '10px'
+            }
+        }else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <= 10) {
+                //criança
+                img.setAttribute('src', 'crianca-fem.png')
+                img.style.marginTop = '10px'
+            }else if (idade < 21) {
+                //jovem
+                img.setAttribute('src', 'mulher-jovem.png')
+                img.style.marginTop = '10px'
+            }else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'mulher-adulta.png')
+                img.style.marginTop = '10px'
+            }else {
+                //idoso
+                img.setAttribute('src', 'idosa-fem.png')
+                img.style.marginTop = '10px'
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
+        res.appendChild(img)
+    }
+
+}
